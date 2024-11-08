4500dae8b1fb3bd4f970eecfe9526d12344eb2e9
Brendt Kohl <kohlb3641@my.uwstout.edu>
2024-11-08 16:14:56 -0600

    Changed how the ListNode handles pointers to next nodes, rewrote createMap(), ~gameboard(), and some others with this in mind.

--- MCLL/EscherMaze.cpp
+++ MCLL/EscherMaze.cpp
@@ -2,30 +2,48 @@
 using namespace std;
 #include "gameboard.h"
 #include <iostream>
-#include <fstream>
 
 // method declarations
 int main()
 {
-	gameboard game;
+	gameboard game{15}; // set starting moves
 	char dir;
 
+	// welcome player to the game 
+	cout << "Welcome user to the maze! You have fallen down a hole in the ground and end up in complete darkness." << endl
+		<< "You must rely on your trusty compass to find the exit." << endl
+		<< "But as a horrible stroke of luck, the ceiling is starting to crumble on top of you, and you have a limited time to find the exit! good luck!" << endl;
+		
 	// setup game map
 	game.createMap();
 	// create local player variable
 	player* plr = game.getPlayer();
 	// create game loop
 	do {
+		// seperate dialogue
+		cout << "-----------------------------------------------------------" << endl;
+		// cout light level of current room
+		switch (plr->getCurPos()->getLightLevel())
+		{
+		case 0:
+			cout << "The room is very dark, you can hardly see anything." << endl;
+			break;
+		case 1:
+			cout << "The room is slightly brighter than complete darkness, you might be on the right track." << endl;
+			break;
+		case 2:
+			cout << "The room is now very bright, you must be close to the exit." << endl;
+			break;
+		}
+
 		cout << "You have " << game.getPlayer()->getNumMoves() << " moves remaining." << endl;
 		cout << "You are in room " << plr->getCurPos()->getNodeID() << ". Where would you like to go? (n,s,e,w), q to quit:";
 		cin >> dir;
 		bool hitGoal = game.movePlayer(dir);
 		if (hitGoal)
 		{
-			cout << "Congratulations! You've found the end in " << 10 - game.getPlayer()->getNumMoves() << " moves." << endl;
+			cout << "!! Congratulations! You've found the end in " << game.getStartingMoves() - game.getPlayer()->getNumMoves() << " moves. !!" << endl;
 			break;
 		}
-		// separate moves
-		cout << "-----------------------------------------------------------" << endl;
 	} while (dir != 'q' && plr->getNumMoves() > 0);
 }
--- MCLL/gameboard.h
+++ MCLL/gameboard.h
@@ -1,34 +1,32 @@
 #pragma once
 // Brendt Kohl, Connor Simon
 #include <iostream>
-#include <ctime>
-// TODO: add add/removing nodes -- done
+#include <string> // for room description
+#include <ctime> // for randomly choosing goal
 // TODO: add descriptions for nodes
 // TODO: add "light level" effect to nodes close to the goal -- optional
-
+using namespace std;
 
 class ListNode
 {
 private:
-	// fields for each direction
-	ListNode* north{ nullptr };
-	ListNode* south{ nullptr };
-	ListNode* east{ nullptr };
-	ListNode* west{ nullptr };
+	// 0 == north, 1 == south, 2 == east, 3 == west
+	ListNode* directions[4];
 
 	// field for data
 	static int counter; // for incrementing nodeID
 	int nodeID;//node ID
-	bool isEventNode;//node 15 and node 8 are event nodes
 	int eventType{ 0 };
+	int lightLevel{ 0 };
 	bool isDeleted{ false };
+	// string variable for node description
+	string roomDescription;
 
 public:
 
 	//basic constructor
-	ListNode(bool isEventNode, int eventType) : nodeID(counter)
+	ListNode(int eventType) : nodeID(counter)
 	{
-		this->isEventNode = isEventNode;
 		this->eventType = eventType;
 		// increment counter
 		++counter;
@@ -37,47 +35,43 @@ public:
 	// default constructor
 	ListNode() {
 		nodeID = 0;
-		isEventNode = false;
 	}
-	// data methods
+
+	// data getters
 	bool removed() { return isDeleted; }
 	int getNodeID() { return nodeID; }
+	int getLightLevel() { return lightLevel; }
+
+	// data setters
 	void setData(int data) { this->nodeID = data; }
 	void setDeleted(bool deleted) { this->isDeleted = deleted; }
+	void setLightLevel(int level) { this->lightLevel = level; }
+	void setDescription(string desc) { this->roomDescription = desc; }
 
 	// access methods for each direction
+	ListNode* getDirections(int index) { return directions[index]; }
 
-	ListNode* getNorth() { return north; }
-	ListNode* getSouth() { return south; }
-	ListNode* getEast() { return east; }
-	ListNode* getWest() { return west; }
-
-	// mutators for each direction
-	void setNorth(ListNode* node) { this->north = node; }
-	void setSouth(ListNode* node) { this->south = node; }
-	void setEast(ListNode* node) { this->east = node; }
-	void setWest(ListNode* node) { this->west = node; }
+	// set direction for given index
+	void setDirections(int index, ListNode* nodeRef) { directions[index] = nodeRef; }
 
 	// event method
 	void onTraverse()
 	{
 		// return if not an event node
-		if (!isEventNode || isDeleted) { return; }
+		if (eventType == 0 || isDeleted) { return; }
 		
 		// determine which event to play
-		if (eventType == 1)
+		if (eventType == 1) // add node event
 		{
-			// Add node
 			// set this east deleted to false
-			this->east->setDeleted(false);
+			directions[2]->setDeleted(false);
 			// cout something
-			std::cout << "After walking into this room, you notice a door to the East magically open." << std::endl;
+			cout << "After walking into this room, you notice a door to the East magically open." << endl;
 		}
-		else
+		else // 2: delete node event
 		{
-			// delete node
 			this->isDeleted = true;
-			std::cout << "You enter the room and notice that the ceiling begins to rain down ontop of you! " << std::endl << "Quickly find an exit!" << std::endl;
+			cout << "You enter the room and notice that the ceiling begins to rain down ontop of you! " << endl << "Quickly find an exit!" << endl;
 		}
 	}
 };
@@ -89,13 +83,14 @@ class player
 {
 private:
 	ListNode* curPos{ nullptr };
-	int numMoves{10};
+	int numMoves;
 
 public:
 
-	player(ListNode* startingPos)
+	player(ListNode* startingPos, int moves)
 	{
 		this->curPos = startingPos;
+		this->numMoves = moves;
 	}
 	ListNode* getCurPos()
 	{
@@ -107,46 +102,33 @@ public:
 	}
 	bool movePlayer(char dir)
 	{
+		int selectedDir;
 		switch (dir)
 		{
 		case 'n':
-			if(curPos->getNorth() == nullptr || curPos->getNorth()->removed()){
-				return false;
-			}
-
-			curPos = curPos->getNorth();
-			--numMoves;
-			return true;
+			selectedDir = 0;
 			break;
 		case 's':
-			if (curPos->getSouth() == nullptr || curPos->getSouth()->removed()) {
-				return false;
-			}
-
-			curPos= curPos->getSouth();
-			--numMoves;
-			return true;
+			selectedDir = 1;
 			break;
 		case 'e':
-			if (curPos->getEast() == nullptr || curPos->getEast()->removed()) {
-				return false;
-			}
-			curPos = curPos->getEast();
-			--numMoves;
-			return true;
-		break;
+			selectedDir = 2; 
+			break;
 		case 'w':
-			if (curPos->getWest() == nullptr || curPos->getWest()->removed()) {
-				return false;
-			}
-			curPos = curPos->getWest();
-			--numMoves;
-			return true;
+			selectedDir = 3;
 			break;
 		default:
+			// no valid direction chosen, return early
 			return false;
 		}
-		
+		// make temp variable for next node reference
+		ListNode* selectedNode = curPos->getDirections(selectedDir);
+		// check if next node is null or if its been deleted, return false early.
+		if (selectedNode == nullptr || selectedNode->removed()) { return false; }
+
+		curPos = selectedNode;
+		--numMoves;
+		return true;
 	}
 };
 
@@ -156,42 +138,51 @@ private:
 	ListNode* center{ nullptr };
 	player* plr{ nullptr };
 	ListNode* goal{ nullptr };
-
+	const int startingMoves;
+	
 public:
-	gameboard()
-	{
-		//TODO: randomize starting node (?)
-	}
+	// default constructor - init moves to 15
+	gameboard() : startingMoves(15) {}
+
+	// optional constructor if the user wants a custom move number
+	gameboard(int moves) : startingMoves(moves) {}
 
 	// Destructor
 	~gameboard()
 	{
-		// TODO find every node and delete them
+		// check all paths, find next highest nodeID, delete node and continue until no paths exist
+		ListNode* nextNode = center;
+		int curID = nextNode->getNodeID();
+		while (curID <= 17)
+		{
+			ListNode* tempNode = nextNode;
+			curID = nextNode->getNodeID();
+
+			// find next node
+			for (int i = 0; i < 4; i++)
+			{
+				ListNode* ProspectedNode = nextNode->getDirections(i);
+				if (ProspectedNode != nullptr && ProspectedNode->getNodeID() == curID + 1)
+				{
+					nextNode = ProspectedNode;
+				}
+			}
+			// no paths found, exit loop to prevent infinite looping
+			if (nextNode == tempNode) { break; }
+			++curID;
+			// delete tempNode
+			delete tempNode;
+		}
 	}
 
 	// getters
-
-	ListNode* getHeadNode()
-	{
-		return center;
-	}
-
-	player* getPlayer()
-	{
-		return plr;
-	}
+	ListNode* getHeadNode() { return center; }
+	player* getPlayer() { return plr; }
+	int getStartingMoves() { return startingMoves; }
 
 	// setters
-
-	void setHeadNode(ListNode* head)
-	{
-		center = head;
-	}
-
-	void setPlayer(player* plr)
-	{
-		this->plr = plr;
-	}
+	void setHeadNode(ListNode* head) { center = head; }
+	void setPlayer(player* plr) { this->plr = plr; }
 
 	bool movePlayer(char dir)
 	{
@@ -201,61 +192,25 @@ public:
 
 		if (success)
 		{
-			std::cout << "You have moved to room " << plr->getCurPos()->getNodeID() << std::endl;
+			cout << "You have moved to room " << plr->getCurPos()->getNodeID() << endl;
 		}
 		else
 		{
-			std::cout << "You hit your head on a hard stone wall. It stings a little." << std::endl;
+			cout << "You hit your head on a hard stone wall. It stings a little." << endl;
 		}
 		// check if player hit the goal, return true
-		cout << "goal pointer: " << goal << " this pointer: " << plr->getCurPos() << endl;
 		if(plr->getCurPos() == goal){
 			return true;
 		}
 		// check if player runs out of moves
 		if (plr->getNumMoves() <= 0) {
-			std::cout << "You have failed to make it out of the maze in time." << std::endl;
+			cout << "You have failed to make it out of the maze in time." << endl;
 			return false;
 		}
-
 		
 		// call onTraverse method in new node
 		plr->getCurPos()->onTraverse();
 		return false;
-	}
-
-	//REMOVE -- early traverse attempt
-	bool traverseTest()
-	{
-		ListNode* nextNode = center;
-		int headID = nextNode->getNodeID();
-		//determine next node
-		while (headID < 16)
-		{
-			headID = nextNode->getNodeID();
-			if (nextNode->getNorth() != nullptr && nextNode->getNorth()->getNodeID() == headID + 1)
-			{
-				nextNode = nextNode->getNorth();
-			}
-			else if (nextNode->getSouth() != nullptr &&  nextNode->getSouth()->getNodeID() == headID + 1)
-			{
-				nextNode = nextNode->getSouth();
-			}
-			else if (nextNode->getEast() != nullptr && nextNode->getEast()->getNodeID() == headID + 1)
-			{
-				nextNode = nextNode->getEast();
-			}
-			else if (nextNode->getWest() != nullptr && nextNode->getWest()->getNodeID() == headID + 1)
-			{
-				nextNode = nextNode->getWest();
-			}
-			else
-			{
-				break;
-			}
-			++headID;
-		}
-		if (headID == 16) return true;
 	}
 
 	void createMap()
@@ -263,178 +218,174 @@ public:
 		// prevent creating the map twice
 		if (center != nullptr) { return; }
 
-		// Create nodes
-		ListNode* node1 = new ListNode(false, 0);
-		ListNode* node2 = new ListNode(false, 0);
-		ListNode* node3 = new ListNode(false, 0);
-		ListNode* node4 = new ListNode(false, 0);
-		ListNode* node5 = new ListNode(false, 0);
-		ListNode* node6 = new ListNode(false, 0);
-		ListNode* node7 = new ListNode(false, 0);
-		ListNode* node8 = new ListNode(true, 2);//delete node event
-		ListNode* node9 = new ListNode(false, 0);
-		ListNode* node10 = new ListNode(false, 0);
-		ListNode* node11 = new ListNode(false, 0);
-		ListNode* node12 = new ListNode(false, 0);
-		ListNode* node13 = new ListNode(false, 0);
-		ListNode* node14 = new ListNode(false, 0);
-		ListNode* node15 = new ListNode(true, 1);//add node event
-		ListNode* node16 = new ListNode(false, 0);
+		ListNode* node1 = new ListNode(0);
+		ListNode* node2 = new ListNode(0);
+		ListNode* node3 = new ListNode(0);
+		ListNode* node4 = new ListNode(0);
+		ListNode* node5 = new ListNode(0);
+		ListNode* node6 = new ListNode(0);
+		ListNode* node7 = new ListNode(0);
+		ListNode* node8 = new ListNode(2);
+		ListNode* node9 = new ListNode(0);
+		ListNode* node10 = new ListNode(0);
+		ListNode* node11 = new ListNode(0);
+		ListNode* node12 = new ListNode(0);
+		ListNode* node13 = new ListNode(0);
+		ListNode* node14 = new ListNode(0);
+		ListNode* node15 = new ListNode(1);
+		ListNode* node16 = new ListNode(0);
+		ListNode* node17 = new ListNode(0);
 		
-		// random added node
-		ListNode* node17 = new ListNode(false, 0);
-		// Set directional pointers
-		node17->setNorth(node5); // node5 E
-		node17->setSouth(node16); // node6 N
-		node17->setEast(node12); // node12 W
-		node17->setWest(node15); // node15 E
-		// set node17 to deleted
-		node17->setDeleted(true);
+		// For node17
+		node17->setDirections(0, node5);  // North -> node5
+		node17->setDirections(1, node16); // South -> node16
+		node17->setDirections(2, node12); // East -> node12
+		node17->setDirections(3, node15); // West -> node15
+		node17->setDeleted(true);        // Set node17 to deleted
 
-		//for node16
-		node16->setNorth(node2);
-		node16->setSouth(node15);
-		node16->setEast(node4);
-		node16->setWest(node11);
+		// For node16
+		node16->setDirections(0, node2);  // North -> node2
+		node16->setDirections(1, node15); // South -> node15
+		node16->setDirections(2, node4);  // East -> node4
+		node16->setDirections(3, node11); // West -> node11
 
 		// For node15
-		node15->setNorth(node16);
-		node15->setSouth(node3);
-		node15->setEast(node17);
-		node15->setWest(node14);
+		node15->setDirections(0, node16); // North -> node16
+		node15->setDirections(1, node3);  // South -> node3
+		node15->setDirections(2, node17); // East -> node17
+		node15->setDirections(3, node14); // West -> node14
 
 		// For node14
-		node14->setNorth(nullptr);
-		node14->setSouth(node2);
-		node14->setEast(node15);
-		node14->setWest(node13);
+		node14->setDirections(0, nullptr); // North -> nullptr
+		node14->setDirections(1, node2);   // South -> node2
+		node14->setDirections(2, node15);  // East -> node15
+		node14->setDirections(3, node13);  // West -> node13
 
 		// For node13
-		node13->setNorth(node12);
-		node13->setSouth(node1);
-		node13->setEast(node14);
-		node13->setWest(node9);
+		node13->setDirections(0, node12);  // North -> node12
+		node13->setDirections(1, node1);   // South -> node1
+		node13->setDirections(2, node14);  // East -> node14
+		node13->setDirections(3, node9);   // West -> node9
 
 		// For node12
-		node12->setNorth(node1);
-		node12->setSouth(node13);
-		node12->setEast(node11);
-		node12->setWest(node17);
+		node12->setDirections(0, node1);   // North -> node1
+		node12->setDirections(1, node13);  // South -> node13
+		node12->setDirections(2, node11);  // East -> node11
+		node12->setDirections(3, node17);  // West -> node17
 
 		// For node11
-		node11->setNorth(node10);
-		node11->setSouth(node12);
-		node11->setEast(node16);
-		node11->setWest(nullptr);
+		node11->setDirections(0, node10);  // North -> node10
+		node11->setDirections(1, node12);  // South -> node12
+		node11->setDirections(2, node16);  // East -> node16
+		node11->setDirections(3, nullptr); // West -> nullptr
 
 		// For node10
-		node10->setNorth(node9);
-		node10->setSouth(node11);
-		node10->setEast(node1);
-		node10->setWest(nullptr);
+		node10->setDirections(0, node9);   // North -> node9
+		node10->setDirections(1, node11);  // South -> node11
+		node10->setDirections(2, node1);   // East -> node1
+		node10->setDirections(3, nullptr); // West -> nullptr
 
 		// For node9
-		node9->setNorth(node4);
-		node9->setSouth(node10);
-		node9->setEast(node13);
-		node9->setWest(node8);
+		node9->setDirections(0, node4);    // North -> node4
+		node9->setDirections(1, node10);   // South -> node10
+		node9->setDirections(2, node13);   // East -> node13
+		node9->setDirections(3, node8);    // West -> node8
 
 		// For node8
-		node8->setNorth(nullptr);
-		node8->setSouth(node9);
-		node8->setEast(node7);
-		node8->setWest(nullptr);
+		node8->setDirections(0, nullptr);  // North -> nullptr
+		node8->setDirections(1, node9);    // South -> node9
+		node8->setDirections(2, node7);    // East -> node7
+		node8->setDirections(3, nullptr);  // West -> nullptr
 
 		// For node7
-		node7->setNorth(nullptr);
-		node7->setSouth(nullptr);
-		node7->setEast(node6);
-		node7->setWest(node8);
+		node7->setDirections(0, nullptr);  // North -> nullptr
+		node7->setDirections(1, nullptr);  // South -> nullptr
+		node7->setDirections(2, node6);    // East -> node6
+		node7->setDirections(3, node8);    // West -> node8
 
 		// For node6
-		node6->setNorth(node17);
-		node6->setSouth(nullptr);
-		node6->setEast(node5);
-		node6->setWest(node7);
+		node6->setDirections(0, node17);   // North -> node17
+		node6->setDirections(1, nullptr);  // South -> nullptr
+		node6->setDirections(2, node5);    // East -> node5
+		node6->setDirections(3, node7);    // West -> node7
 
 		// For node5
-		node5->setNorth(node4);
-		node5->setSouth(nullptr);
-		node5->setEast(node17);
-		node5->setWest(node6);
+		node5->setDirections(0, node4);    // North -> node4
+		node5->setDirections(1, nullptr);  // South -> nullptr
+		node5->setDirections(2, node17);   // East -> node17
+		node5->setDirections(3, node6);    // West -> node6
 
 		// For node4
-		node4->setNorth(node3);
-		node4->setSouth(node5);
-		node4->setEast(node9);
-		node4->setWest(node16);
+		node4->setDirections(0, node3);    // North -> node3
+		node4->setDirections(1, node5);    // South -> node5
+		node4->setDirections(2, node9);    // East -> node9
+		node4->setDirections(3, node16);   // West -> node16
 
 		// For node3
-		node3->setNorth(node15);
-		node3->setSouth(node4);
-		node3->setEast(nullptr);
-		node3->setWest(node2);
+		node3->setDirections(0, node15);   // North -> node15
+		node3->setDirections(1, node4);    // South -> node4
+		node3->setDirections(2, nullptr);  // East -> nullptr
+		node3->setDirections(3, node2);    // West -> node2
 
 		// For node2
-		node2->setNorth(node14);
-		node2->setSouth(node16);
-		node2->setEast(node3);
-		node2->setWest(node1);
+		node2->setDirections(0, node14);   // North -> node14
+		node2->setDirections(1, node16);   // South -> node16
+		node2->setDirections(2, node3);    // East -> node3
+		node2->setDirections(3, node1);    // West -> node1
 
-		//for node1
-		node1->setNorth(node13);
-		node1->setSouth(node12);
-		node1->setEast(node2);
-		node1->setWest(node10);
+		// For node1
+		node1->setDirections(0, node13);   // North -> node13
+		node1->setDirections(1, node12);   // South -> node12
+		node1->setDirections(2, node2);    // East -> node2
+		node1->setDirections(3, node10);   // West -> node10
 
+
+		/////////////////////////////////////
 		// set headNode to node1
-		center = node1;
 
 		// create player starting at node1
-		plr = new player(node1);
+		plr = new player(node1, startingMoves);
 		
 		// determine random goal node
 		srand(time(nullptr));
-		int goalID = rand() % 16 + 2;
+
+		
+		int goalID = rand() % 15 + 2;
 		if (goalID == 8) { goalID++; }
-		//std::cout << "DEBUG -- GOAL NODE IS: " << goalID << std::endl;
-
-		ListNode* nextNode = node1;
-		int headID = nextNode->getNodeID();
-		while (headID <= 16)
+		center = node1;
+		////////////////
+		ListNode* nextNode = center;
+		int curID = nextNode->getNodeID();
+		// find goal node from random curID
+		while (goalID != curID)
 		{
-			headID = nextNode->getNodeID();
-			if (headID == goalID)
+			// find next node
+			for (int i = 0; i < 4; i++)
 			{
-				goal = nextNode;
-				break;
+				ListNode* ProspectedNode = nextNode->getDirections(i);
+				if (ProspectedNode != nullptr && ProspectedNode->getNodeID() == curID + 1)
+				{
+					nextNode = ProspectedNode;
+					break;
+				}
 			}
-
-			if (nextNode->getNorth() != nullptr && nextNode->getNorth()->getNodeID() == headID + 1)
-			{
-				nextNode = nextNode->getNorth();
-			}
-			else if (nextNode->getSouth() != nullptr && nextNode->getSouth()->getNodeID() == headID + 1)
-			{
-				nextNode = nextNode->getSouth();
-			}
-			else if (nextNode->getEast() != nullptr && nextNode->getEast()->getNodeID() == headID + 1)
-			{
-				nextNode = nextNode->getEast();
-			}
-			else if (nextNode->getWest() != nullptr && nextNode->getWest()->getNodeID() == headID + 1)
-			{
-				nextNode = nextNode->getWest();
-			}
-			else
-			{
-				// if no paths exist, break loop, set goal to center
-				goal = center;
-				break;
-			}
-			++headID;
+			// update curID
+			curID = nextNode->getNodeID();
 		}
+		goal = nextNode;
+		//////////////////
 		// set light level of outside nodes
-		// set light level of farther outside nodes
+		for (int i = 0; i < 4; i++)
+		{
+			if (goal->getDirections(i) == nullptr) { continue; }
+			goal->getDirections(i)->setLightLevel(2);
+			// set light level of outside outside nodes
+			for (int j = 0; j < 4; j++)
+			{
+				if (goal->getDirections(i)->getDirections(j) == nullptr) { continue; }
+				goal->getDirections(i)->getDirections(j)->setLightLevel(1);
+			}
+		}
+
 	}
 };
